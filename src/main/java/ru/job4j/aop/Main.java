01b224be5173da9e130fd8e763c575d3a743cfdc
Veronika Lapenok <veronika.lapenok.yr@gmail.com>
2023-09-05 13:00:25 +0300

    AOP concept [#505076]

---
+++ src/main/java/ru/job4j/aop/Main.java
@@ -0,0 +1,18 @@
+package ru.job4j.aop;
+
+import org.apache.log4j.BasicConfigurator;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.job4j.aop.config.Config;
+import ru.job4j.aop.model.Comment;
+import ru.job4j.aop.service.ProcessComment;
+
+public class Main {
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
+        ProcessComment process = context.getBean("processComment", ProcessComment.class);
+        process.publishingComment(new Comment("this is a comment", "Petr"));
+        process.deleteComment(1);
+        context.close();
+    }
+}
