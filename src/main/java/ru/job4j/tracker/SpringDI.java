21ce0850e31db1144190dff64bd23c15d4dc30b0
Veronika Lapenok <veronika.lapenok.yr@gmail.com>
2023-10-27 16:04:22 +0300

    2. Scanning the project [#1070 #455482]

--- src/main/java/ru/job4j/tracker/SpringDI.java
+++ src/main/java/ru/job4j/tracker/SpringDI.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class SpringDI {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.scan("ru.job4j.di.tracker");
+        context.scan("ru.job4j.tracker");
         context.refresh();
         StartUI ui = context.getBean(StartUI.class);
 
